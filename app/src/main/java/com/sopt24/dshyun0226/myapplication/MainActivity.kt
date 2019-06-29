cdebb7e52750df1423a7d63dfef7d36f6ae96f16
Jeon Sumin <bubble_030@khu.ac.kr>
2019-06-29 15:37:14 +0900

    Add variable named SuminJeon

--- app/src/main/java/com/sopt24/dshyun0226/myapplication/MainActivity.kt
+++ app/src/main/java/com/sopt24/dshyun0226/myapplication/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val SuminJeon = "I love SOPT24 Android Part!!"
     }
 }
