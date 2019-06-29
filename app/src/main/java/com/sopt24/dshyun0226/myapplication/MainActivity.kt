e218789238eac9170f7d695cdd653e0a635234ac
leesohee <shfksthgml@naver.com>
2019-06-29 16:06:41 +0900

    Add variable name sohee with comment

--- app/src/main/java/com/sopt24/dshyun0226/myapplication/MainActivity.kt
+++ app/src/main/java/com/sopt24/dshyun0226/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sohee : String = "Git study!! Android 최공!"
+        val sohee : String = "Git study!!"
     }
 }
